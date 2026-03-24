91615ddd246ae90c40e219ee8780778be823f9e9
bek1F <xamidullayevbehruz1@gmail.com>
2026-03-24 16:38:27 +0500

    section two

--- src/components/SectionTwo/index.jsx
+++ src/components/SectionTwo/index.jsx
@@ -1,12 +1,41 @@
-
+import img1 from '../../assets/imgtwo1.png';
+import img2 from '../../assets/imgtwo2.png';
+import img3 from '../../assets/imgtwo3.png';
 import './style.css'
 
 
 function SectionTwo() {
     return (
-        <>
-            <h3>SectionTwo</h3>
-        </>
+        <div className='conainer'>
+            <div className='top'>
+                <h2 className='topH2'>Immigration made easy</h2>
+                <p className='topP'>Urelocation simplifies the visa process for you and your international employees. We’ll handle the fine details so you can focus on other tasks</p>
+                <div className='section'>
+                    <div className='uel' >
+                        <h3 className='toph3'>Fast Service</h3>
+                        <p className='topp2'>We research, build, submit, and get approved visas in a fraction of the time while working around your timelines.</p>
+                    </div>
+                    <img src={img1} alt="" />
+                </div>
+                <div className='section'>
+                    <img src={img2} alt="" />
+                    <div className='uel' >
+                        <h3 className='toph3'>Full control and visibility</h3>
+                        <p className='topp2'>Our intuitive platform guides you and your employee through every step of the immigration process, with at-a-glance status updates along the way.</p>
+                    </div>
+
+                </div>
+                <div className='section'>
+                    <div className='uel' >
+                        <h3 className='toph3'>Personalized case managers</h3>
+                        <p className='topp2'>Feel confident with our team of global mobility experts. They’re on hand to answer all your questions, letting you focus on what really matters.</p>
+                    </div>
+                    <img src={img3} alt="" />
+                </div>
+
+
+            </div>
+        </div>
     );
 }
 
