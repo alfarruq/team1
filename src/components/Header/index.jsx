1c6e805371d59b46cf297df22ff721250e0f4ba7
Azizxoja-a <abdulazizovazizhoza@gmail.com>
2026-03-24 17:05:20 +0500

    header section one

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,20 @@
 
 import './style.css'
-
+import img from '../../assets/hed.png'
 
 function Header() {
     return (
         <>
-            <h3>Header</h3>
+            <header>
+                <img src={img} alt="Header Image" />
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                <button>Take a scoring test</button>
+            </header>
         </>
     );
 }
--- src/components/SectionOne/index.jsx
+++ src/components/SectionOne/index.jsx
@@ -1,11 +1,18 @@
 
 import './style.css'
+import img from '../../assets/odcha.png'
 
-
-function SectionOne() {
+function SectionOne() { 
     return (
         <>
-            <h3>SectionOne</h3>
+            <div className='section-one'>
+                <div className='ss1'>
+                    <h1>One platform meeting <span>all your relocation</span> needs</h1> <br />
+                    <p>Get a Global Talent Visa and relocate to the UK. Start your journey now</p> <br />
+                    <button className='but'>Take a scoring test</button>
+                </div>
+                <img src={img} alt="" />
+            </div>  
         </>
     );
 }
